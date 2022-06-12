8a1353aa2422f53f81ccb9f087b9ac04647307d2
parimiadarsh <aparimi@hawk.iit.edu>
2022-06-12 18:14:31 -0400

    errors

--- src/main/java/com/example/TexasHamburgComp/TexasHamburgCompApplication.java
+++ src/main/java/com/example/TexasHamburgComp/TexasHamburgCompApplication.java
@@ -1,14 +1,15 @@
 package com.example.TexasHamburgComp;
 
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "OpenApiTitle",version = "1.0",description = "OpenAPIdemo"))
+@EnableSwagger2
 public class TexasHamburgCompApplication {
 
 	public static void main(String[] args) {
